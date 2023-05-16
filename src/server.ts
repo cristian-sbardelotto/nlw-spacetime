ac3758a5456bbb70160c3cf2c467f8692066f436
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-05-16 18:31:54 -0300

    feat(get): created the endpoint to return the users

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+const prisma = new PrismaClient();
 
 const port = 3333;
 
-app.get('/hello', () => {
-  return 'hello world!';
+app.get('/users', async () => {
+  const users = await prisma.user.findMany();
+  
+  return users;
 });
 
 app
