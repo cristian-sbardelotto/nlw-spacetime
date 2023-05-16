149da7d9694dc097458ccd5a2761feee921b4993
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-05-15 21:56:52 -0300

    feat(boilerplate): setup the environment and installed prisma, fastify and eslint libs

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+const port = 3333;
+
+app.get('/hello', () => {
+  return 'hello world!';
+});
+
+app
+  .listen({ port })
+  .then(() => console.log(`🚀 Server running on http://localhost:${port}`));
