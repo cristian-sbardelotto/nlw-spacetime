d535905d8e431dcc97ba2214a4cff627fc5f7df4
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-05-17 15:11:25 -0300

    feat(cors): configured

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
 import fastify from 'fastify';
 
+import cors from '@fastify/cors';
+
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify();
 const port = 3333;
 
+app.register(cors, {
+  origin: true,
+});
 app.register(memoriesRoutes);
 
 app
