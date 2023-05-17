6d8d6575fe2f3f43be4fc1eb49ddebcf12f7a673
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-05-17 20:47:18 -0300

    feat(login): created auth route and basic login funcionality

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
 
 import cors from '@fastify/cors';
 
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 const port = 3333;
@@ -11,6 +14,7 @@ app.register(cors, {
   origin: true,
 });
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app
   .listen({ port })
