fc9d7173db61f721477e8e7610577aa3c7e94496
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-05-20 10:32:20 -0300

    feat(upload): implemented the backend route to upload media files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 import 'dotenv/config';
 
 import fastify from 'fastify';
-
 import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
+import multipart from '@fastify/multipart';
 
 import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
+import { uploadRoutes } from './routes/upload';
+
+import { resolve } from 'node:path';
 
 const app = fastify();
 const port = 3333;
+
+app.register(multipart);
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+});
 
 app.register(cors, {
   origin: true,
@@ -21,6 +31,7 @@ app.register(jwt, {
 
 app.register(memoriesRoutes);
 app.register(authRoutes);
+app.register(uploadRoutes);
 
 app
   .listen({ port })
